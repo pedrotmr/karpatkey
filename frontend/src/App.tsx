58a5e706fe271e63933b03df04bdfd93386ba651
Pedro Ramos <pedrotr@hotmail.com>
2024-08-22 11:33:13 +0100

    chore: add tailwind and prettier configurations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,14 @@
-export default function App() {
-  return <div>hi my name is Pedro</div>;
-}
+const App = () => {
+  return (
+    <>
+      <h1 className="">Hello world!</h1>
+      <p>
+        We help decentralised organisations preserve capital through
+        state-of-the-art risk management and trust-minimised DeFi treasury
+        execution.
+      </p>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "k-black": "#232323",
+        "off-white": "#eeeded",
+        "dark-grey": "#6B6B6B",
+        "mid-grey": "#bdbdbd",
+        "light-grey": "#d9d9d9",
+        "k-green": "#54B9A1",
+        "k-red": "#DF5C64",
+        "k-orange": "#F0B065",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
